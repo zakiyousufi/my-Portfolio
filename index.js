490fa16cc8ffb10d1eb92b130b8ae6465f4046f9
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-06-16 18:29:35 +0430

    js file updated

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const hamburger = document.querySelector(".menu")
 const mobMenu = document.querySelector(".mobileMenu")
-const links = document.querySelector("m-menu-text")
+const classOff = document.querySelector(".close")
+const links = document.querySelector(".m-menu-text")
 
 hamburger.addEventListener("click", () => {
     mobMenu.classList.toggle("active");
     
+})
+classOff.addEventListener("click", () => {
+    mobMenu.classList.remove("active")
 })
 links.addEventListener("click", () => {
     mobMenu.classList.remove("active");
