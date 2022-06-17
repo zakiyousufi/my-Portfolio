c93bc291e43c30eb051696301b51271fd2b82363
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-06-18 03:32:17 +0430

    project cards added

--- js/index.js
+++ js/index.js
@@ -1,9 +1,64 @@
-const hamburger = document.querySelector('.menu');
-const mobMenu = document.querySelector('.mobileMenu');
-const classOff = document.querySelector('.close');
-const links = document.querySelector('.m-menu-text');
+let arr = [
+  {
+    name: 'Profesional Art Printing Data',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard'
+  },
+  {
+    name: 'Data Dashboard Healthcare',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard'
+  },
+  {
+    name: 'Website Protfolio',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.A daily selection of privately personalized r haspersonalized r has been the industrys standard'
+  },
+  {
+    name: 'Profesional Art Printing Data',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard'
+  },
+  {
+    name: 'Data Dashboard Healthcare',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard'
+  },
+  {
+    name: 'Website Protfolio',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.A daily selection of privately personalized r haspersonalized r has been the industrys standard'
+  },
+];
+
+function displayProjects() {
+  const projectCards = document.getElementById('cards');
+  const cards = document.createElement('div');
+  cards.classList.add('projects-section2');
+  arr.forEach((project) => {
+    const projectHTML =
+      `
+        <div class="page2"><h2 class="heading2 heading4 text-color h2-margin">${project.name}</h2>
+        <p class="project-discription project-discription2 text-color">${project.description}</p>
+        <ul class="web-icons web-icons4">
+          <li>
+            <button type="submit" class="web-icon-page2 icon-web">html</button>
+          </li>
+          <li>
+           <button type="submit" class="web-icon-page2 icon-web">bootstrap</button>
+          </li>
+          <li>
+           <button type="submit" class="web-icon-page2 icon-web">Ruby</button>
+          </li>
+        </ul>
+        <button type="submit" class="btn-active animate-btn button4 button2 button1-text">
+          See Project
+        </button></div>`;
+    cards.insertAdjacentHTML('beforeend', projectHTML);
+  });
+  projectCards.appendChild(cards);
+};
 
 window.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.querySelector('.menu');
+  const mobMenu = document.querySelector('.mobileMenu');
+  const classOff = document.querySelector('.close');
+  const links = document.querySelector('.m-menu-text');
+
   hamburger.addEventListener('click', () => {
     mobMenu.classList.toggle('active');
   });
@@ -13,17 +68,18 @@ window.addEventListener('DOMContentLoaded', () => {
   links.addEventListener('click', () => {
     mobMenu.classList.remove('active');
   });
+
+  displayProjects();
 });
 
-const seeProject = document.querySelector('.btn-active');
-const popup = document.querySelector('.popup');
-const popupClose = document.querySelector('.popup-close');
+const seeProject = document.querySelectorAll('.btn-active');
+  const popup = document.querySelector('.popup');
+  const popupClose = document.querySelector('.popup-close');
 
-window.addEventListener('DOMContentLoaded', () => {
-  seeProject.addEventListener('click', () => {
+  seeProject.forEach(addEventListener('click', () => {
     popup.classList.toggle('active');
-  });
+  }));
+
   popupClose.addEventListener('click', () => {
     popup.classList.remove('active');
   });
-});
