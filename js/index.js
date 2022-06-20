0a559f88cdfa25f32f9d48e45202e263740fbb73
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-06-20 14:51:02 +0430

    Email input makes validated so it can't select upercase alphabets

--- js/index.js
+++ js/index.js
@@ -111,6 +111,12 @@ const handleSubmit = (event) => {
   }
 };
 
+function setItemsData (event) {
+  const inputName = event.target.id;
+  const { value } = document.getElementById(inputName);
+  localStorage.setItem(inputName, value);
+}
+
 window.addEventListener('DOMContentLoaded', () => {
   const hamburger = document.querySelector('.menu');
   const mobMenu = document.querySelector('.mobileMenu');
